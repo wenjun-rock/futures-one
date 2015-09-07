eab35d33eac0077f525aec90c195aa83b5f95cd0
fanwenjun <fanwenjun>
2015-09-08 01:42:04 +0800

    Donchian

--- futures-davinci/src/main/java/cn/fwj/futures/data/strategy/trend/donchian/DonchianFounder.java
+++ futures-davinci/src/main/java/cn/fwj/futures/data/strategy/trend/donchian/DonchianFounder.java
@@ -1,5 +1,7 @@
 package cn.fwj.futures.data.strategy.trend.donchian;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,8 @@ import cn.fwj.futures.data.enu.Product;
 import cn.fwj.futures.data.launch.AbstractBaseLaunch;
 import cn.fwj.futures.data.repository.KLineRepo;
 import cn.fwj.futures.data.strategy.trend.donchian.struct.DonchianTrend;
+import cn.fwj.futures.data.strategy.trend.donchian.struct.DonchianWave;
+import cn.fwj.futures.data.strategy.trend.donchian.struct.DonchianWave.Direction;
 import cn.fwj.futures.data.struct.DailyKLine;
 
 @Component
@@ -19,10 +23,27 @@ public class DonchianFounder extends AbstractBaseLaunch {
 
 	@Override
 	protected void execute() throws Exception {
-		DailyKLine kLine = kLineRepo.loadDailyKLine(Product.HuangJin);
-//		DonchianTrend trend = new DonchianTrendBuilder("2015-06-16", "2016-01-01", 20, 10).createDonchianTrend(kLine);
-		DonchianTrend trend = new DonchianTrendBuilder("2015-05-18", "2016-01-01", 20, 10).createDonchianTrend(kLine);
-		System.out.println(JSON.toJSONString(trend));
+
+		for (Product product : Product.values()) {
+			DailyKLine kLine = kLineRepo.loadDailyKLine(product);
+			DonchianTrend trend = new DonchianTrendBuilder("2005-01-01", "2016-01-01", 20, 10)
+					.createDonchianTrend(kLine);
+
+			BigDecimal total = BigDecimal.ZERO;
+			int size = 0;
+			for (DonchianWave wave : trend.getHistWave()) {
+				if (wave.getExitPrice() == null) {
+					continue;
+				}
+				size++;
+				if (wave.getDirection() == Direction.UP) {
+					total = total.add(wave.getExitPrice()).subtract(wave.getEnterPrice());
+				} else if (wave.getDirection() == Direction.DOWN) {
+					total = total.add(wave.getEnterPrice()).subtract(wave.getExitPrice());
+				}
+			}
+			System.out.println(String.format("%s\t%s\t%s", product, size, total));
+		}
 	}
 
 	public static void main(String[] args) {
