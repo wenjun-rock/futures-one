42e3b5098decc448d3eda76f19e76b00f01bef96
wenjun.fan <wenjun.fan@LT-310.howbuy.domain>
2015-09-14 18:34:04 +0800

    K线数据导出

--- futures-davinci/src/main/java/fwj/futures/data/strategy/hedging/model/ExportOrigin.java
+++ futures-davinci/src/main/java/fwj/futures/data/strategy/hedging/model/ExportOrigin.java
@@ -1,24 +1,62 @@
 package fwj.futures.data.strategy.hedging.model;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.io.Files;
+
 import fwj.futures.data.enu.ProdEnum;
 import fwj.futures.data.launch.AbstractBaseLaunch;
-import fwj.futures.data.process.DataProcessor;
+import fwj.futures.data.process.DataURI;
+import fwj.futures.resource.entity.KLine;
+import fwj.futures.resource.repository.KLineRepository;
 
 @Component
 public class ExportOrigin extends AbstractBaseLaunch {
 
 	@Autowired
-	private DataProcessor dataProcessor;
+	private KLineRepository kLineRepository;
+	
+	@Autowired
+	private DataURI dataURI;
 
 	@Override
 	protected void execute() throws Exception {
-		dataProcessor.exportEndPrice("2014-01-01", "2016-09-02", ProdEnum.JiaoTan, ProdEnum.JiaoMei);
+		this.exportEndPrice("2015-01-01", "2015-09-02", ProdEnum.JiaoTan, ProdEnum.JiaoMei);
 	}
 
 	public static void main(String[] args) {
 		launch(ExportOrigin.class);
+	}
+
+	public void exportEndPrice(String startDt, String endDt, ProdEnum... prods) throws Exception {
+		exportEndPrice(startDt, endDt, Stream.of(prods).map(prod -> prod.getCode()).collect(Collectors.toList()));
+	}
+
+	public void exportEndPrice(String startDt, String endDt, List<String> codeList) throws Exception {
+		Collections.sort(codeList);
+
+		String head = codeList.stream().reduce("DATE", (l, r) -> l + "," + r);
+		Map<String, List<KLine>> kLineMap = codeList.stream()
+				.map(code -> kLineRepository.findByCodeDateRange(code, startDt, endDt))
+				.flatMap(kLineList -> kLineList.stream())
+				.collect(Collectors.groupingBy(KLine::getDt, Collectors.toList()));
+		String content = kLineMap.entrySet().stream().filter(entry -> entry.getValue().size() == codeList.size())
+				.sorted((l, r) -> l.getKey().compareTo(r.getKey())).map(entry -> {
+					String dt = entry.getKey();
+					return entry.getValue().stream().sorted((l, r) -> l.getCode().compareTo(r.getCode()))
+							.map(kLine -> kLine.getEndPrice().toString()).reduce(dt, (l, r) -> l + "," + r);
+				}).reduce(head, (l, r) -> l + "\n" + r);
+		
+		String fileName = startDt + "_" + endDt + "_" + String.join("_", codeList);
+		Files.asCharSink(dataURI.getModelFile(fileName), StandardCharsets.UTF_8).write(content);
 	}
 }
--- futures-davinci/src/main/java/fwj/futures/resource/repository/KLineRepository.java
+++ futures-davinci/src/main/java/fwj/futures/resource/repository/KLineRepository.java
@@ -1,5 +1,7 @@
 package fwj.futures.resource.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +12,9 @@ public interface KLineRepository extends JpaRepository<KLine, Integer> {
 
 	@Query("select max(o.dt) from KLine o where o.code = :code")
 	String findMaxDtByCode(@Param("code") String code);
+
+	@Query("select o from KLine o where code = :code and (dt between :startDt and :endDt)")
+	List<KLine> findByCodeDateRange(@Param("code") String code, @Param("startDt") String startDt,
+			@Param("endDt") String endDt);
 
 }
