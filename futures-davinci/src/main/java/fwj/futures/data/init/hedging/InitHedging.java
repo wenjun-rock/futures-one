a3f9bbcdafef73981c3f2570cad6a84680acdd7a
wenjun.fan <wenjun.fan@LT-310.howbuy.domain>
2015-10-09 13:02:33 +0800

    修改套利设置

--- futures-davinci/src/main/java/fwj/futures/data/init/hedging/InitHedging.java
+++ futures-davinci/src/main/java/fwj/futures/data/init/hedging/InitHedging.java
@@ -1,8 +1,7 @@
 package fwj.futures.data.init.hedging;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,67 +23,48 @@ public class InitHedging extends AbstractBaseLaunch {
 	@Override
 	protected void execute() throws Exception {
 
-		List<Hedging> hedgingList = new ArrayList<>();
-
-		Hedging hedging1 = new Hedging();
-		hedging1.setName("豆油棕榈油套利1");
-		hedging1.setDesc("");
-		hedging1.setUpLimit(new BigDecimal(300));
-		hedging1.setDownLimit(new BigDecimal(-300));
-		hedging1.setExpression(JSON.toJSONString(Formula.create().putConstant("-587.08732")
-				.putMultinomial(ProdEnum.DouYou, "1").putMultinomial(ProdEnum.ZongLvYou, "-1.03179")));
-		hedgingList.add(hedging1);
-		System.out.println(JSON.toJSONString(hedging1));
-		
-	
-		Hedging hedging3 = new Hedging();
-		hedging3.setName("焦炭焦煤套利1");
-		hedging3.setDesc("");
-		hedging3.setUpLimit(new BigDecimal(30));
-		hedging3.setDownLimit(new BigDecimal(-30));
-		hedging3.setExpression(JSON.toJSONString(Formula.create().putConstant("162.31")
-				.putMultinomial(ProdEnum.JiaoTan, "1").putMultinomial(ProdEnum.JiaoMei, "-1.60584")));
-		hedgingList.add(hedging3);
-		System.out.println(JSON.toJSONString(hedging3));
-		
-		Hedging hedging4 = new Hedging();
-		hedging4.setName("豆油棕榈油套利2");
-		hedging4.setDesc("");
-		hedging4.setUpLimit(new BigDecimal(300));
-		hedging4.setDownLimit(new BigDecimal(-300));
-		hedging4.setExpression(JSON.toJSONString(Formula.create().putConstant("2881.57")
-				.putMultinomial(ProdEnum.DouYou, "-1").putMultinomial(ProdEnum.ZongLvYou, "0.5619586")));
-		hedgingList.add(hedging4);
-		System.out.println(JSON.toJSONString(hedging4));
-		
-		Hedging hedging5 = new Hedging();
-		hedging5.setName("豆油棕榈油套利3");
-		hedging5.setDesc("");
-		hedging5.setUpLimit(new BigDecimal(300));
-		hedging5.setDownLimit(new BigDecimal(-300));
-		hedging5.setExpression(JSON.toJSONString(Formula.create().putConstant("3162.88")
-				.putMultinomial(ProdEnum.DouYou, "-1.430981").putMultinomial(ProdEnum.ZongLvYou, "1")));
-		hedgingList.add(hedging5);
-		System.out.println(JSON.toJSONString(hedging5));
-		
-
-		for (Hedging ne : hedgingList) {
-			Hedging old = hedgingRepository.findByName(ne.getName());
-			if (old == null) {
-				hedgingRepository.save(ne);
-			} else {
-				old.setName(ne.getName());
-				old.setDesc(ne.getDescription());
-				old.setUpLimit(ne.getUpLimit());
-				old.setDownLimit(ne.getDownLimit());
-				old.setExpression(ne.getExpression());
-				hedgingRepository.save(old);
+		Stream.of(//
+				new Input("Y-1*P", "-600", ProdEnum.DouYou, "1", ProdEnum.ZongLvYou, "-1", 300, -300),
+				new Input("J-1.5*JM", "100", ProdEnum.JiaoTan, "1", ProdEnum.JiaoMei, "-1.5", 30, -30),
+				new Input("A-2*C", "0", ProdEnum.DaDou1, "1", ProdEnum.YuMi, "-2", 300, -300)
+		//
+		).forEach(input -> {
+			Hedging hedging = hedgingRepository.findByName(input.name);
+			if (hedging == null) {
+				hedging = new Hedging();
 			}
-		}
-
+			hedging.setExpression(JSON.toJSONString(Formula.create().putConstant(input.constant)
+					.putMultinomial(input.prod1, input.coefficient1).putMultinomial(input.prod2, input.coefficient2)));
+			hedging.setUpLimit(new BigDecimal(input.upLimit));
+			hedging.setDownLimit(new BigDecimal(input.downLimit));
+			hedgingRepository.save(hedging);
+		});
 	}
 
 	public static void main(String[] args) {
 		launch(InitHedging.class);
+	}
+
+	private class Input {
+		private String name;
+		private String constant;
+		private ProdEnum prod1;
+		private String coefficient1;
+		private ProdEnum prod2;
+		private String coefficient2;
+		private int upLimit;
+		private int downLimit;
+
+		public Input(String name, String constant, ProdEnum prod1, String coefficient1, ProdEnum prod2,
+				String coefficient2, int upLimit, int downLimit) {
+			this.name = name;
+			this.constant = constant;
+			this.prod1 = prod1;
+			this.coefficient1 = coefficient1;
+			this.prod2 = prod2;
+			this.coefficient2 = coefficient2;
+			this.upLimit = upLimit;
+			this.downLimit = downLimit;
+		}
 	}
 }
