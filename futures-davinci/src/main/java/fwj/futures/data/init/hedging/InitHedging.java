89c82cbea240449adf98d70971df02f6fdf006f3
Frank <Frank@Home>
2015-10-13 07:28:38 +0800

    add

--- futures-davinci/src/main/java/fwj/futures/data/init/hedging/InitHedging.java
+++ futures-davinci/src/main/java/fwj/futures/data/init/hedging/InitHedging.java
@@ -50,10 +50,12 @@ public class InitHedging extends AbstractBaseLaunch {
 				new Input("菜油-豆油", ProdEnum.CaiYou, "1", ProdEnum.DouYou, "-1"),
 				new Input("菜油-棕榈油", ProdEnum.CaiYou, "1", ProdEnum.ZongLvYou, "-1"),
 				new Input("大豆-玉米", ProdEnum.DaDou1, "1", ProdEnum.YuMi, "-2"),
+				new Input("淀粉-玉米", ProdEnum.YuDian, "1", ProdEnum.YuMi, "-1"),
 				
 				new Input("焦炭-焦煤", ProdEnum.JiaoTan, "1", ProdEnum.JiaoMei, "-1.5"),
 				new Input("热卷-螺纹", ProdEnum.ReJuan, "1", ProdEnum.LuoWenGang, "-1"),
 				new Input("螺纹-焦炭", ProdEnum.LuoWenGang, "1", ProdEnum.JiaoTan, "-2"),
+				new Input("螺纹-动力煤", ProdEnum.LuoWenGang, "1", ProdEnum.DongLiMeiZC, "-8"),
 				new Input("玻璃-铁矿石", ProdEnum.BoLi, "1", ProdEnum.TieKuangShi, "-1"),
 				new Input("玻璃-焦煤", ProdEnum.BoLi, "1", ProdEnum.JiaoMei, "-1"),
 				new Input("螺纹-玻璃", ProdEnum.LuoWenGang, "1", ProdEnum.BoLi, "-3"),
@@ -62,8 +64,10 @@ public class InitHedging extends AbstractBaseLaunch {
 				new Input("PTA-螺纹", ProdEnum.PTA, "1", ProdEnum.LuoWenGang, "-2"),
 				
 				new Input("塑料-PP", ProdEnum.SuLiao, "1", ProdEnum.JuBinXi, "-1"),
+				new Input("PP-甲醇", ProdEnum.JuBinXi, "1", ProdEnum.JiaChun, "-2"),
 				
-				new Input("沪锌-沪铅", ProdEnum.Xin, "1", ProdEnum.Qian, "-1")
+				new Input("沪锌-沪铅", ProdEnum.Xin, "1", ProdEnum.Qian, "-1"),
+				new Input("沪镍-沪锌", ProdEnum.Nie, "1", ProdEnum.Xin, "-10")
 		//
 		).forEach(input -> {
 			Hedging hedging = hedgingRepository.findByName(input.name);
